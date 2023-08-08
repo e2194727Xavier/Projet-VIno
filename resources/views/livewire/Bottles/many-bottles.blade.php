b410a1cfbb2c62376461fe809f598ce14e4d70b8
maryline888 <maryline888@gmail.com>
2023-08-08 12:31:47 -0400

    mise a jour css

--- resources/views/livewire/Bottles/many-bottles.blade.php
+++ resources/views/livewire/Bottles/many-bottles.blade.php
@@ -3,11 +3,11 @@
       
         <div class="max-w-1200px">
             @foreach($bottles as $bottle)
-                <article class="mx-6 my-8 flex border-2 border-gold rounded-lg items-center gap-2">
-                    <img src="{{ $bottle->image }}" alt="{{ $bottle->name }}" class="max-w-80">
+                <article class="mx-4 my-2 flex border-2 border-gold rounded-lg items-center gap-2">
+                    <img src="{{ $bottle->image }}" alt="{{ $bottle->name }}" class="p-2 max-w-80">
                     <div class="flex flex-col flex-grow justify-end items-end p-4 sm:flex-row sm:justify-between sm:gap-4">
-                        <h1 class="text-right font-bold font-roboto">{{ $bottle->name }}</h1>
-                        <p class="text-xs mt-2 mb-2">{{ $bottle->description }}</p>
+                        <h1 class="text-right font-bold font-roboto text-l">{{ $bottle->name }}</h1>
+                        <p class="text-right text-xs mt-2 mb-2">{{ $bottle->description }}</p>
                         <!-- <button wire:click="saveUnlistedBottle" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Ajouter</button> -->
 
                     </div>
