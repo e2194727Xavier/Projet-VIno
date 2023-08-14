8d8fb6e119d8a1e3c8dddc7e6858de6450009d00
busseauc77 <busseauc@gmail.com>
2023-08-14 11:09:30 -0400

    changement du message de bienvenue

--- resources/views/livewire/Layouts/index.blade.php
+++ resources/views/livewire/Layouts/index.blade.php
@@ -9,9 +9,9 @@
     <!-- ceci apparaitra tant que l'usager n'a qu'un cellier -->
     @if(count($cellars) < 2) 
     <section class="my-6 ">
-        <p class="mb-2 font-roboto font-bold text-lg">Votre premier cellier est créer!</p>
-        <p class="">Vous pouvez parcourir le catalogue de nos bouteilles ou utiliser l'outil de recherche </p>
-        <p>Notre catalogue contient plus de 8000 sorte de vin différents.</p>
+        <p class="mb-2 font-roboto font-bold text-lg">Votre premier cellier est créé, ajoutez-y vos bouteilles et gérez-y son inventaire!</p>
+        <p class="">Parcourez notre catalogue contenant plus de 8000 vins différents ou ajoutez vos bouteilles personnalisées.</p>
+        <p>N'hésitez pas à utiliser la barre de recherche pour trouver facilement ce que vous cherchez. Vous pouvez aussi utiliser la recherche avancée pour trier par catégorie, par pays ou par prix.</p>
     </section>
     @endif
 
